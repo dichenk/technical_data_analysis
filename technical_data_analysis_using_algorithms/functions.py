e85e231ab863e2f0ffdef1cf411711fdfc1c0523
dichenk <dchenk@DESKTOP-R5OCG73.localdomain>
2022-12-11 22:27:50 +0300

    binary search and quicksort done

--- technical_data_analysis_using_algorithms/functions.py
+++ technical_data_analysis_using_algorithms/functions.py
@@ -74,49 +74,114 @@ def get_by_date2(date = date_sec, name = name_sec):
         writer = csv.writer(file)
         writer.writerows(a)
 '''
-
-
 ## to show that I can rwite down an algo
 def quick_sort(qbject, column):
-    pd_1 = pd.DataFrame(columns = list(qbject.columns))
-    pd_2 = pd.DataFrame(columns = list(qbject.columns))
-    pd_2.loc[len(pd_2)] = qbject.iloc[0]
-    pd_3 = pd.DataFrame(columns = list(qbject.columns))
-    for i in range(1, 10000):        
-        if qbject.at[i, column] < pd_2.at[0, column]:
-            pd_1.loc[len(pd_1)] = qbject.loc[i]
-        elif qbject.at[i, column] > pd_2.at[0, column]:
-            pd_3.loc[len(pd_3)] = qbject.loc[i]
-            if check(i): return cheet_sort(qbject, column) ## call sorthing with pandas
+    ## делим будем делить таблицу на три части
+    pd_1 = pd.DataFrame(columns = list(qbject.columns)) ## больше искомого элемента
+    pd_2 = qbject.iloc[[0]] ## равно
+    pd_3 = pd.DataFrame(columns = list(qbject.columns)) ## меньше
+
+    if len(qbject) == 1: return qbject ## выход из рекурсии
+
+    for i in range(len(qbject)):
+        print(i)
+        if qbject.iloc[i][column] < qbject.iloc[0][column]:
+            pd_1 = pd.concat([pd_1, qbject.iloc[[i]]])
+        elif qbject.iloc[i][column] > qbject.iloc[0][column]:
+            pd_3 = pd.concat([pd_3, qbject.iloc[[i]]])
+            if check(i, qbject): 
+                return cheet_sort(qbject, column) ## call sorthing with pandas
         else:
-            pd_2.loc[len(pd_2)] = qbject.loc[i]
-    pd_1 = pd.concat([quick_sort(pd_1, column) , pd_2])
-    pd_1 = pd.concat([pd_1, quick_sort(pd_3, column)])
+            pd_2 = pd.concat([pd_2, qbject.iloc[[i]]])
+    try:
+        pd_1 = pd.concat([quick_sort(pd_1, column), pd_2])
+    except:
+        pd_1 = pd.concat([pd_1, pd_2])
+    try:
+        pd_1 = pd.concat([pd_1, quick_sort(pd_3, column)])
+    except:
+        pd_1 = pd.concat([pd_1, pd_3])
     return pd_1
 
 def cheet_sort(qbject, column):
-    return qbject.sort_values(by = [column])
-## check how long out algo is going to work
-def check(i):
-    global time_flag, start_time, big_len
-    if time_flag and i > 1000:
-        how_time = time.time() - start_time
-        big_o = big_len * math.log(big_len)
-        how_long = (big_o - i) / how_time
+    print(9)
+    qbject = qbject.sort_values(by = column)
+    qbject.to_csv('cheet.csv')
+    return qbject
 
+## check how long out algo is going to work
+def check(i, qbject):
+    global time_flag, start_time
+    if time_flag and i > 400:
+        how_time = time.time() - start_time
+        big_o = len(qbject) * math.log(len(qbject))
+        how_long = (big_o / i) * how_time
         print(f'Алгоритм работает уже {how_time} секунд')
         print('Осталось работать примерно:')
         print(f'{how_long} секунд или {how_long / 60} минут или {how_long / 3600} часов или {how_long / 3600 / 24} дней')
         print('Введите 1, если не хотите ждать')
         a = input('Что решили? ')
         if a == "1": return True
-        else: return False
         time_flag = False
 
-df = pd.read_csv('all_stocks_5yr.csv') ## reading csv
-big_len = len(df)
-time_flag = True ## just a flag
-start_time = time.time() ## program started
-dj = quick_sort(df, 'open')
-print(dj[0:3])
+## binary searsh
+def search(qbject, column, value):
+    print('searching')
+    i = 0
+    j = len(qbject) - 1
+    k = 0
+    while 1:
+        k = (j - i) // 2 + i
+        if qbject.iloc[k][column] == value:
+            break
+        elif qbject.iloc[k][column] < value:
+            i = k
+        else:
+            j = k
+    return k
 
+def slice(qbject, column, value):
+    k = search(qbject, column, value)
+    i = k
+    j = k
+    while qbject.iloc[i][column] == value:
+        i -= 1
+        if -1 == i: break
+    while qbject.iloc[j][column] == value:
+        j += 1
+        if len(qbject) == j: break
+    return (i + 1, j - 1)
+
+def get_by_date(qbject = None, date = 0, name = 0, val_d = 0, val_n = 0):
+    if date == 0 and name == 0:
+        return qbject
+    elif date == 0:
+        return get_info(qbject, name, val_n)
+    elif name == 0:
+        qbject = get_info(qbject, date, val_d)
+        print(qbject)
+        return qbject
+    else:
+        qbject = get_info(qbject, date, val_d)
+        print(qbject)
+        time_flag = True
+        start_time = time.time()
+        qbject = get_info(qbject, name, val_n)
+        print(qbject)
+        return qbject
+
+def get_info(qbject, name, value):
+        df_1 = quick_sort(qbject, name)
+        a = slice(df_1, name, value)
+        if a[0] == a[1]: df_1 = df_1.iloc[a[0]] ## slice по диапазону i:1+1 почему-то возвращает пустой датафрейм
+        else: df_1 = df_1.iloc[a[0]:a[1]]
+        return df_1
+
+df = pd.read_csv('all_stocks_5yr.csv') ## reading csv
+time_flag = True ## just a flag
+start_time = start_time_2 = time.time() ## program started
+dj = get_by_date(df, 'date', "Name", '2016-06-14', 'A')
+dj.to_csv('result.csv')
+
+print(dj)
+print(f'время выполнения программы - {(time.time() - start_time_2) / 60} минут')
